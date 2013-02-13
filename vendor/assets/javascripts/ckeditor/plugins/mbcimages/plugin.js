58368e900921febd1d04536f22d049eaa31c84f3
David Patrick <dpatrick@lightspeedsystems.com>
2013-02-13 02:20:54 -0800

    custom plugins

---
+++ vendor/assets/javascripts/ckeditor/plugins/mbcimages/plugin.js
@@ -0,0 +1,32 @@
+CKEDITOR.plugins.add('mbcimages',
+{
+  init: function(editor)
+  {
+
+    var $mediaBrowser;
+    $mediaBrowser = $("<div><div class='loading-dialog' style='width:inherit; height:inherit'></div></div>").dialog({
+      title: 'Insert Image',
+      modal: true,
+      autoOpen: false,
+      height: 360,
+      width: 600
+     });
+
+    editor.addCommand('insertMBCImage',
+    {
+      exec : function(editor)
+      {
+        $mediaBrowser.dialog("open");
+        $mediaBrowser.load("/documents/get_images/?ckinstance=" + CKEDITOR.currentInstance.name + '' );
+      }
+    });
+    
+    editor.ui.addButton('MBCImage',
+    {
+      label   : 'Insert My Big Campus Image',
+      command : 'insertMBCImage',
+      icon    : 'image'
+    });
+
+  }
+});
---
+++ vendor/assets/javascripts/ckeditor/plugins/mbclibrary/plugin.js
@@ -0,0 +1,32 @@
+CKEDITOR.plugins.add('mbclibrary',
+{
+  init: function(editor)
+  {
+
+    var $mediaBrowser;
+    $mediaBrowser = $("<div><div class='loading-dialog' style='width:inherit; height:inherit'></div></div>").dialog({
+      title: 'MBC Library Videos',
+      modal: true,
+      autoOpen: false,
+      height: 510,
+      width: 676
+     });
+    
+    editor.addCommand('insertMBCLibrary',
+    {
+      exec : function(editor)
+      {
+        $mediaBrowser.dialog("open");
+        $mediaBrowser.load("/items/video_search/?library_videos=" + CKEDITOR.currentInstance.name + '');
+      }
+    });
+
+    editor.ui.addButton('MBCLibrary',
+    {
+      label   : 'Insert My Big Campus Library Video',
+      command : 'insertMBCLibrary',
+      icon    : '/assets/application/ckeditor-youtube-icon.png'
+    });
+
+  }
+});
