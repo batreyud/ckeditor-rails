60177c1d3c4e40426c0f9030c2f574bfc3eb2715
David Patrick <dpatrick@lightspeedsystems.com>
2013-05-28 19:51:20 -0700

    adding animation on closing existing open panel

--- vendor/assets/javascripts/ckeditor/plugins/mbcimages-modal/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/mbcimages-modal/plugin.js
@@ -2,13 +2,18 @@ CKEDITOR.plugins.add('mbcimages-modal',
 {
   init: function(editor)
   {
-
     editor.addCommand('insertModalMBCImage',
     {
       exec : function(editor)
       {
-        $(".modal-content").append("<div class='media-selector' style='width:5px;'>Loading media...</div>")
-        $('.media-selector').animate({'width': '250px'}, 1000);
+        if ($(".modal-content").length){
+          $('.media-selector').animate({'width': '1px'}, 1000, function(){
+            $('.media-selector').html('Loading media...');
+          });
+        }else{
+          $(".modal-content").append("<div class='media-selector' style='width:0px;'>Loading media...</div>")
+        }
+        $('.media-selector').animate({'width': '200px'}, 1000);
         $(".media-selector").load("/documents/get_images/?ckinstance=" + CKEDITOR.currentInstance.name + '' );
       }
     });
--- vendor/assets/javascripts/ckeditor/plugins/mbcvideos-modal/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/mbcvideos-modal/plugin.js
@@ -2,13 +2,18 @@ CKEDITOR.plugins.add('mbcvideos-modal',
 {
   init: function(editor)
   {
-
     editor.addCommand('insertModalMBCVideo',
     {
       exec : function(editor)
       {
-        $(".modal-content").append("<div class='media-selector' style='width:5px;'>Loading media...</div>")
-        $('.media-selector').animate({'width': '250px'}, 1000);
+        if ($(".modal-content").length){
+          $('.media-selector').animate({'width': '1px'}, 1000, function(){
+            $('.media-selector').html('Loading media...');
+          });
+        }else{
+          $(".modal-content").append("<div class='media-selector' style='width:0px;'>Loading media...</div>")
+        }
+        $('.media-selector').animate({'width': '200px'}, 1000);
         $(".media-selector").load("/items/video_search/?my_videos=" + CKEDITOR.currentInstance.name + '');
       }
     });
