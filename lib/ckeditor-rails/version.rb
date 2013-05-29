ae811a650da16ae0f5a7a5784799ce82a6f71699
David Patrick <dpatrick@lightspeedsystems.com>
2013-05-28 20:22:01 -0700

    forgot to check in images

--- lib/ckeditor-rails/version.rb
+++ lib/ckeditor-rails/version.rb
@@ -1,6 +1,6 @@
 module Ckeditor
   module Rails
-    VERSION = '4.0.7.2'
+    VERSION = '4.0.7.3'
     EDITOR_VERSION = '4.0.1'
   end
 end
--- vendor/assets/javascripts/ckeditor/plugins/mbcimages-modal/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/mbcimages-modal/plugin.js
@@ -6,15 +6,16 @@ CKEDITOR.plugins.add('mbcimages-modal',
     {
       exec : function(editor)
       {
-        if ($(".modal-content").length){
+        if ($('.media-selector').length){
           $('.media-selector').animate({'width': '1px'}, 1000, function(){
             $('.media-selector').html('Loading media...');
           });
         }else{
-          $(".modal-content").append("<div class='media-selector' style='width:0px;'>Loading media...</div>")
-        }
-        $('.media-selector').animate({'width': '200px'}, 1000);
-        $(".media-selector").load("/documents/get_images/?ckinstance=" + CKEDITOR.currentInstance.name + '' );
+          $(".modal-content").append("<div class='media-selector' style='width:0px;'>Loading media...</div>");
+        };
+        $('.media-selector').animate({'width': '200px'}, 1000, function(){
+          $('.media-selector').load("/documents/get_images/?ckinstance=" + CKEDITOR.currentInstance.name + '' );
+        });
       }
     });
 
